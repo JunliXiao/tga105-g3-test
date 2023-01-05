6054694c2fdd246cd3b187ad3754ce9608c64ba6
Boris Hong <mionrus@gmail.com>
2023-01-05 16:53:03 +0800

    add branch test

--- src/main/java/Boris.java
+++ src/main/java/Boris.java
@@ -3,6 +3,21 @@ public class Boris {
 
 	public static void main(String[] args) {
 		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
+		System.out.println("新年快樂");
 
 	}
 
