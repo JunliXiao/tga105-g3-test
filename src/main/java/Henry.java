378337dfc38bb40394d4daa7233eb023487541b6
g23821208 <g23821208@gmail.com>
2023-01-05 16:52:35 +0800

    koaola

--- src/main/java/Henry.java
+++ src/main/java/Henry.java
@@ -3,7 +3,8 @@ public class Henry {
 
 	public static void main(String[] args) {
 
-		System.out.println("我還不想寫JDBC TT");
+		System.out.println("JDBC QQ");
+		System.out.println("JDBC TT");
 	}
 
 }
